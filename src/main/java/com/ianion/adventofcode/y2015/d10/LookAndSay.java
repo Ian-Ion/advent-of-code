b501396a3ff89d82e70b812b157770839e4f6a92
Ian-Ion <ian.jones@ocado.com>
2024-04-04 10:21:18 +0100

    2015 Day 10 Part 1

---
+++ src/main/java/com/ianion/adventofcode/y2015/d10/LookAndSay.java
@@ -0,0 +1,89 @@
+package com.ianion.adventofcode.y2015.d10;
+
+import lombok.Builder;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+@Slf4j
+@Builder(toBuilder = true)
+public record LookAndSay(
+        String sequence
+) {
+
+    private static final Pattern CONSECUTIVE_DIGITS = Pattern.compile("(\\d)\\1*");
+
+    public static LookAndSay play(Game game) {
+        return LookAndSay.initialize(game.sequence).playMoreRounds(game.rounds);
+    }
+
+    private static LookAndSay initialize(String sequence) {
+        return LookAndSay.builder()
+                .sequence(sequence)
+                .build();
+    }
+
+    private LookAndSay playMoreRounds(int rounds) {
+        return rounds == 0 ? this
+                : playOneRound().playMoreRounds(rounds - 1);
+    }
+
+    private LookAndSay playOneRound() {
+        Matcher currentSequenceMatcher = CONSECUTIVE_DIGITS.matcher(sequence);
+        String nextSequence = currentSequenceMatcher.find()
+                ? generateNextSequence(currentSequenceMatcher.reset())
+                : sequence;
+
+        return this.toBuilder().sequence(nextSequence).build();
+    }
+
+    private String generateNextSequence(Matcher currentSequenceMatcher) {
+        return Stream
+                .iterate(
+                        NextSequenceBuilder.initialize(currentSequenceMatcher),
+                        NextSequenceBuilder::isNotFinished,
+                        NextSequenceBuilder::parseNextRunOfDigits)
+                .reduce((first, second) -> second)
+                .map(NextSequenceBuilder::getNextSequence).orElse("");
+    }
+
+    @Builder
+    public record Game(
+            String sequence,
+            int rounds
+    ) {}
+
+    @Builder(toBuilder = true)
+    private record NextSequenceBuilder(
+            Matcher currentSequenceMatcher,
+            StringBuilder nextSequence
+    ) {
+
+        private static NextSequenceBuilder initialize(Matcher matcher) {
+            return NextSequenceBuilder.builder()
+                    .currentSequenceMatcher(matcher)
+                    .nextSequence(new StringBuilder())
+                    .build();
+        }
+
+        private boolean isNotFinished() {
+            return currentSequenceMatcher.find();
+        }
+
+        private NextSequenceBuilder parseNextRunOfDigits() {
+            String nextDigit = currentSequenceMatcher.group(0);
+            return this.toBuilder()
+                    .nextSequence(
+                            nextSequence
+                                    .append(nextDigit.length())
+                                    .append(nextDigit.charAt(0)))
+                    .build();
+        }
+
+        private String getNextSequence() {
+            return nextSequence.toString();
+        }
+    }
+}
---
+++ src/test/java/com/ianion/adventofcode/y2015/d10/LookAndSayTest.java
@@ -0,0 +1,34 @@
+package com.ianion.adventofcode.y2015.d10;
+
+import com.ianion.adventofcode.common.FileLoader;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LookAndSayTest {
+
+    @ParameterizedTest
+    @MethodSource("playTestArgs")
+    void testPlay(String input, int rounds, int expectedFinalSequenceLength) {
+        LookAndSay.Game game = LookAndSay.Game.builder().sequence(input).rounds(rounds).build();
+
+        LookAndSay result = LookAndSay.play(game);
+
+        assertThat(result.sequence()).hasSize(expectedFinalSequenceLength);
+    }
+
+    private static Stream<Arguments> playTestArgs() {
+        return Stream.of(
+                Arguments.of("1", 1, 2),
+                Arguments.of("1", 2, 2),
+                Arguments.of("1", 3, 4),
+                Arguments.of("1", 4, 6),
+                Arguments.of("1", 5, 6),
+                Arguments.of(FileLoader.readFileAsString("src/test/resources/inputs/y2015/d10/input.txt"), 40, 329356)
+        );
+    }
+}
